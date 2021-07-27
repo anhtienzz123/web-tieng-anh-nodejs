00b064eb5ebe1a077bb1fa8b7bea17199c332e46
Tien Huynh <anhtienzz123@gmail.com>
2021-07-27 11:02:23 +0700

    add api crawl blog

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const wordRouter = require("./word");
 const courseRouter = require("./course");
 const examRouter = require("./exam");
 const videoRouter = require("./video");
+const blogRouter = require("./blog");
 
 const route = (app) => {
   app.use("/words", wordRouter);
   app.use("/courses", courseRouter);
   app.use("/exams", examRouter);
   app.use("/videos", videoRouter);
+  app.use("/blogs", blogRouter);
 };
 
 module.exports = route;
